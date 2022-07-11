9922d9a6c9841ee66d203ca5336c128f6088c358
Uzor Collins <uzorcllns@gmail.com>
2022-07-11 12:17:28 +0000

    Added file field for image upload to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,7 +20,7 @@ def create
 end
 private
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
   end
   
 end
